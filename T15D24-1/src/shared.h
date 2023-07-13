88a333b0d84b2e02cfc3193a429d3b446723f596
Ilya Lopatin <75094158+lekabot@users.noreply.github.com>
2023-07-13 07:51:14 +0700

    Add files via upload

---
+++ T15D24-1/src/shared.h
@@ -0,0 +1,11 @@
+#ifndef SRC_SHARED_H_
+#define SRC_SHARED_H_
+
+int modulesControll();
+int showTables();
+int getChoice(int gap1, int gap2);
+int levelsControll();
+int eventsControll();
+void printMenu(const char* table_name);
+void clearInputBuffer();
+#endif  // SRC_SHARED_H_
---
+++ T15D24-1/src/status_events.h
@@ -0,0 +1,20 @@
+#ifndef SRC_STATUS_EVENTS_H_
+#define SRC_STATUS_EVENTS_H_
+
+#include "libs.h"
+
+int checkEventsId(FILE* ptr, int id);
+int getLastEventsId(FILE* ptr);
+int print_events(FILE* ptr);
+events getEventsRecord(int id);
+int addEventsRecord(FILE* ptr, events rec);
+int deleteEventsRecord(FILE* ptr, int id);
+int changeEventsRecord(FILE* ptr, int id, events rec);
+int insertEventsRecord(FILE* ptr, int id, events rec);
+int selectEvents(FILE* ptr, int count);
+
+int getDate(char* date);
+int getTime(char* time);
+char* getStr();
+
+#endif  // SRC_STATUS_EVENTS_H_
